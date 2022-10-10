76268b231939569f47ffe2b3394e007685f8fd43
Osamacapa <majinantonio16@gmail.com>
2022-10-10 08:25:35 -0500

    first commit

---
+++ pagina1.py
@@ -0,0 +1,33 @@
+#VARIABLES
+
+# a=1
+# b=2
+
+# resultado=a+b
+
+#---------------------
+
+# a=("Hola")
+# b=("Hola")
+
+#EN ESTE CASO LAS DOS VARIABLES TIENEN LA MISMA DIRECCION
+#PERO
+
+# a=("Hola!")
+# b=("Hola!")
+
+#SI TIENEN UN CARACTER DE EXPRESION DIFERENTE DE LETRA O NUMERO, TENDRAN DIFERENTES DIRECCIONES
+
+#--------------------
+
+# ASIGNACION DE VARIABLES
+
+# mensaje = ("Hola")
+# mensaje += (" ")
+# mensaje += ("Juan")
+# print(mensaje)
+
+# mensaje =("Hola")
+# espacio =(" ")
+# nombre =("Juan")
+# print (mensaje+espacio+nombre)
